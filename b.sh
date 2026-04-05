1ac20143cc3c69014369cad2d1216acd9d00c306
kevinkepha <kephakev@gmail.com>
2026-04-06 00:25:45 +0300

    added modification to server

--- b.sh
+++ b.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial FastAPI Docker setup"
 git branch -M main
-git remote add origin git@github.com:yourusername/fastapi-demo.git
+git remote add origin git@github.com:kevinkepha/fastapi-demo.git
 git push -u origin main
