5374caa3a5b984da76e371ddf4190f5f2a9463b5
kevinkepha <kephakev@gmail.com>
2026-04-06 00:02:50 +0300

    added app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
             <head>
                 <meta charset='UTF-8'>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                <title>FastAPI Tube</title>
+                <title> Tube</title>
                 <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' rel='stylesheet'>
                 <style>
                     body { background-color: #181818; color: #fff; }
